2df0257304409f368602cca0584e2c62c6465b39
iamtor9 <iamtor9@gmail.com>
2019-09-01 00:54:46 -0500

    test 1

--- assets/train.js
+++ assets/train.js
@@ -1,147 +1,69 @@
-function fillTable() {
-    let table = document.getElementById("tTable");
-    let row = table.insertRow(0);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
+function myFunction() {
 
-    cell1.innerHTML = document.getElementById("trainName").value;
-    cell2.innerHTML = document.getElementById("dest").value;
-    cell3.innerHTML = document.getElementById("freq").value;
-    cell4.innerHTML = document.getElementById("nextA").value;
-    cell5.innerHTML = document.getElementById("minA").value;
-    return false;
-  }
+  //Train function form inputs  
+  let trainname = document.getElementById("tname").value;
+  let finalDestination = document.getElementById("dest").value;
+  let frequency = document.getElementById("freq").value;
+  let firstTime = document.getElementById("first").value;
+  let firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  
+  // Current Time moment.js & future train
+  let currentTime = moment();
+  let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  let tRemainder = diffTime % frequency;
+  let tMinutesTillTrain = frequency - tRemainder;
+  let nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-//  Button for adding trains
-//document.getElementsByIdName("button").onclick
-let FormData()
-document.getElementById(trainName);
-document.getElementById(dest);
-document.getElementById(freq);
-document.getElementById(nextA);
-document.getElementById(minA);
+  //console log all above
+    console.log(trainname);
+    console.log(finalDestination);
+    console.log(frequency);
+    console.log(moment(nextTrain).format("hh:mm"));
+    console.log(tMinutesTillTrain);
 
-document.getElementById("submitbtn").on("click", function()){
-.push(FormData).value.append("tBody");
+let tReference = document.getElementById('traintb').getElementsByTagName('tbody')[0];
+
+// Insert a row in the table at the last row
+let newTRow   = tReference.insertRow();
+
+// Insert cell for rows starting at index 0
+let newTC1  = newTRow.insertCell(0);
+let newTC2  = newTRow.insertCell(1);
+let newTC3  = newTRow.insertCell(2);
+let newTC4  = newTRow.insertCell(3);
+let newTC5  = newTRow.insertCell(4);
+
+// text appends to cell
+let newUserText  = document.createTextNode(trainname);
+newTC1.appendChild(newUserText);
+
+let newUserText  = document.createTextNode(finalDestination);
+newTC2.appendChild(newUserText);
+
+let newUserText  = document.createTextNode(frequency);
+newTC3.appendChild(newUserText);
+
+let newUserText  = document.createTextNode(moment(nextTrain).format("hh:mm"));
+newTC4.appendChild(newUserText);
+
+let newUserText  = document.createTextNode(tMinutesTillTrain);
+newTC5.appendChild(newUserText);
+
+// local forage code
+let data = {
+       trainname: trainname,
+       finalDestination: finalDestination,
+       firstTime: firstTime,
+       frequency: frequency,
+   }
+   console.log(data);
+   localforage.getItem("saved").then(function(result){
+     if(!result){
+       result = [];
+       result.push(data);
+     }
+     localforage.setItem("saved", result).then(function(){
+         console.log("saved")
+         console.log(result);
+        })}); 
 }
-
-console.log(fillTable);
-
-// have user input
-
-// Creates local "temporary" object for holding train data
-let newTrain = {
-		name:  trainName,
-		destination: destination,
-        frequency: frequency,
-        arrival: nextArrival
-        minAway: minutesAway,
-	}
-
-	// Uploads train data to the database
-	trainData.ref().push(newTrain);
-
-	// Log "newtrain array" to console
-	console.log(newTrain.name);
-	console.log(newTrain.destination);
-	console.log(newTrain.frequency);
-    console.log(newTrain.nextArrival);
-    console.log(newTrain.minutesAway);
-
-// Assumptions
-let tFrequency = 3;
-// Time is 3pm
-let firstTime = "03:00";
-// First Time (pushed back 1 year to make sure it comes before current time)
-let firstTimeConverted = moment(firstTime, "03:00mm").subtract(1, "years");
-console.log(firstTimeConverted);
-// Current Time
-let currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-// Difference between the times
-let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
-// Time apart (remainder)
-let tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
-// Minute Until Train
-let tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-// Next Train
-let nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-//to do: add moment.js info
-
-// code notes below, was brainstorming
-
-//let trainName = $("#trainNameInput").val().trim();
-//let destination = $("#destinationInput").val().trim();
-//let firstTrainUnix = moment(document.getElementById("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
-//let frequency = document.getElementById("#frequencyInput").val().trim();
-
-//create basic array for form inputs
-
-//let array = ["0", "1", "2", "3", "4"]
-
-//let array = document.getElementsByClassName("form-control");
-
-//for (let i = 0; i < array.length; i++) {
-    //const element = array i];
-    
-//}
-//function submitB () {
-    
-//}
-
-//let tdDiv = document.createElementByTagName("td");
-//let node = document.createTextNode("");
-//tdDiv.appendChild(node);
-
-//let formDiv = document.getElementById("submitbtn");
-//formDiv.appendChild(tdDiv);
-
-
-//create javascript elements and then attach them to the html "like" elements
-
-//create a click listener for submit button
-//grab values from basic array 
-//attach each value into it's own td element
-
-// function that gets form data input
-//<input type="text" class="form-control" id="formgroupinput" placeholder="">
-
-//array.push(item1, item2, ..., itemX)
-
-
-//document.createElement("tr");
-
-//for (let i = 0; i < array.length; i++) {
-    //const element = array[i];
-    
-///}
-//create new element called tr
-//append tr to document.getelementbyid(tbody)
- 
-
-
-
-
-
-
-
-
-//<button type="button" onclick="myFunction()">Try it</button>
-
-//<p id="demo"></p>
-
-//<script>
-//function myFunction() {
-  //let x = document.getElementById("myTextarea").form.id;
-  //document.getElementById("demo").innerHTML = x;
-//}
-
-//have function return form data to train times container
